8db0580515e3062d4d6c76bb6258a6c3196f4659
MauricioMiranda6030 <mauri6030@gmail.com>
2025-02-06 17:57:38 -0300

    base del proyecto

---
+++ src/main/java/com/stock/control/StockControlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stock.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockControlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockControlProjectApplication.class, args);
+    }
+
+}
