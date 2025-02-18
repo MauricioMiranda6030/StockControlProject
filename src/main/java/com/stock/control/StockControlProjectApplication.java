450f056db5123962941be7b5638ce7f2a72034f1
MauricioMiranda6030 <mauri6030@gmail.com>
2025-02-18 20:03:46 -0300

    Implementación de javafx, cambios en el main de la app, creación de main.fxml, carpeta front con contralador de archivos fxml, cambios en app.properties para la no inicialización del servidor tomcat, main futura a cambiar de nombre por "stock" o similar, registro de productos visible implementado con su filtro de busqueda, proximo añadir formulario de registro.

--- src/main/java/com/stock/control/StockControlProjectApplication.java
+++ src/main/java/com/stock/control/StockControlProjectApplication.java
@@ -1,13 +1,38 @@
 package com.stock.control;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class StockControlProjectApplication {
+public class StockControlProjectApplication extends Application {
 
+    public static ConfigurableApplicationContext context;
     public static void main(String[] args) {
-        SpringApplication.run(StockControlProjectApplication.class, args);
+        launch();
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        //iniciar spring
+        context = SpringApplication.run(StockControlProjectApplication.class);
+
+        //settear el fxml inicial
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                getClass().getResource("/com/stock/control/front/component/main.fxml")
+        );
+
+        //unificar controlladores de spring y javafx
+        fxmlLoader.setControllerFactory(context::getBean);
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
+        stage.show();
+    }
 }
