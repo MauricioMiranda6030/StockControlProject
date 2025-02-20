4288135c9a63f626e3ab477cff2f4c7475d80d47
Mauricio Miranda <mauri6030@gmail.com>
2025-02-20 02:03:41 -0300

    boton y nuevo form
    boton que abre el nuevo form para productos nuevos, proximo a reutilizar en modificar y con su correspondiente controlador

---
+++ src/main/java/com/stock/control/front/FormProductController.java
@@ -0,0 +1,15 @@
+package com.stock.control.front;
+
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class FormProductController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
