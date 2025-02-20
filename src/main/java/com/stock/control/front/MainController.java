547f288fb9c118deeab0c10067d2a90effa859f3
Mauricio Miranda <mauri6030@gmail.com>
2025-02-20 02:03:41 -0300

    boton y nuevo form
    boton que abre el nuevo form para productos nuevos, proximo a reutilizar en modificar y con su correspondiente controlador

--- src/main/java/com/stock/control/front/MainController.java
+++ src/main/java/com/stock/control/front/MainController.java
@@ -4,16 +4,22 @@ import com.stock.control.entity.Product;
 import com.stock.control.service.IProductService;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import javafx.event.ActionEvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -44,6 +50,9 @@ public class MainController implements Initializable {
     @FXML
     private TextField txtBusqueda;
 
+    @FXML
+    private Button btnAddProduct;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -60,6 +69,24 @@ public class MainController implements Initializable {
         getProducts();
     }
 
+    @FXML
+    public void openFormProduct(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource(
+                "/com/stock/control/front/component/form_product.fxml"
+        ));
+        /* cierra la ventana actual tambien */
+        //Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        /*No cierra la venta actual*/
+        Stage stage = new Stage();
+        stage.setTitle("Nuevo Producto");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    @FXML
     public void getProducts() {
         tableProducts.setItems(FXCollections.observableArrayList(productService.getAllProducts()));
     }
