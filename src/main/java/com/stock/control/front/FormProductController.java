63a5c96e3014f1833ad81fc9a8d8043b1c08a975
Mauricio Miranda <mauri6030@gmail.com>
2025-02-21 20:29:49 -0300

    Hasta aca anda bien
    implementando un fxml loader de spring por que el nuevo no reconoce el service porque no le estoy pasando los beans, asi que implementare un fxmlloader

--- src/main/java/com/stock/control/front/FormProductController.java
+++ src/main/java/com/stock/control/front/FormProductController.java
@@ -1,11 +1,14 @@
 package com.stock.control.front;
 
+import com.stock.control.entity.Product;
+import com.stock.control.service.IProductService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -13,6 +16,9 @@ import java.util.ResourceBundle;
 
 @Component
 public class FormProductController implements Initializable {
+
+    @Autowired
+    private IProductService productService;
 
     @FXML
     private TextArea txtDescription;
@@ -29,9 +35,37 @@ public class FormProductController implements Initializable {
     @FXML
     private Button btnClose;
 
+    @FXML
+    private Button btnSave;
+
+    private Product product;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        product = new Product();
+    }
 
+    @FXML
+    private void saveProduct(){
+
+        String name = txtName.getText();
+        String description = txtDescription.getText();
+        Double price = Double.valueOf(txtPrice.getText());
+        Integer stock = Integer.valueOf(txtStock.getText());
+
+        if(name.isBlank() || stock == null || price == null || description.isBlank())
+            //todo mensaje de llenar campos
+            System.out.println("hace algo lope");
+        else{
+            product.setName(name);
+            product.setPrice(price);
+            product.setStock(stock);
+            product.setDescription(description);
+
+            productService.saveProduct(product);
+            resetTextFields();
+            System.out.println("se guardo");
+        }
     }
 
     @FXML
