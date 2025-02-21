a3d0f5fafee2594ecc4ca54e2a4d447ac2115e53
Mauricio Miranda <mauri6030@gmail.com>
2025-02-20 22:28:20 -0300

    Form Producto
    se sigue avanzando en el form de producto con un par de funcionalidades como reset de fields y cerrar la ventana actual

--- src/main/java/com/stock/control/front/FormProductController.java
+++ src/main/java/com/stock/control/front/FormProductController.java
@@ -1,6 +1,11 @@
 package com.stock.control.front;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -8,8 +13,38 @@ import java.util.ResourceBundle;
 
 @Component
 public class FormProductController implements Initializable {
+
+    @FXML
+    private TextArea txtDescription;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    private TextField txtStock;
+
+    @FXML
+    private Button btnClose;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    @FXML
+    private void resetTextFields(){
+        txtDescription.clear();
+        txtName.clear();
+        txtPrice.clear();
+        txtStock.clear();
+    }
+
+    @FXML
+    private void closeThisForm(){
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
     }
 }
