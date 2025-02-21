2f2c7bbc9570c4ca32ed5af03e68183f8ca371e5
Mauricio Miranda <mauri6030@gmail.com>
2025-02-20 22:28:20 -0300

    Form Producto
    se sigue avanzando en el form de producto con un par de funcionalidades como reset de fields y cerrar la ventana actual

--- src/main/java/com/stock/control/front/MainController.java
+++ src/main/java/com/stock/control/front/MainController.java
@@ -6,7 +6,6 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
